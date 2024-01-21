07c5457a6708a357710a28fd03fa899f8ef73f3a
JNCamil <juancamvh@hotmail.com>
2024-01-21 23:57:27 +0100

    MVC

--- index.php
+++ index.php
@@ -9,11 +9,14 @@ $controlador = new UsuarioController();
 // $controlador->crear();
 
 //Para hacerlo dinámico:
-if($_GET['action'] && method_exists($controlador, $_GET['action'])){
+if(isset($_GET['action']) && method_exists($controlador, $_GET['action'])){
     $action = $_GET['action'];
 
     $controlador->$action();
+}else{
+    echo "La página no existe";
 }
 
-
+/*
+ la función method_exists se utiliza para comprobar si un método específico está definido en una clase o si existe en un objeto dado. Esta función toma dos parámetros: el primer parámetro es el nombre de la clase o un objeto, y el segundo parámetro es el nombre del método que se desea verificar.*/
 ?>
