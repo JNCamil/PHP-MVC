177ae7b221c55daac7bf00b9dc73b78d7dc92229
JNCamil <juancamvh@hotmail.com>
2024-01-26 22:23:11 +0100

    mysql and database

---
+++ config/database.php
@@ -0,0 +1,21 @@
+<?php
+
+class database
+{
+    public static function conectar()
+    {
+
+        #Setear los resultados de la base de datos en utf8 (tildes, ñ, ..)
+        $options = array(
+            PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"
+        );
+
+        $bd = new PDO('mysql:dbname=notas_master;host=localhost', "root", "", $options);
+        #Excepciones para cuando ocurra un error
+        $bd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        return $bd;
+
+    }
+}
+
+?>
---
+++ notas_master.sql
@@ -0,0 +1,24 @@
+CREATE DATABASE notas_master;
+use notas_master;
+
+CREATE TABLE `Usuarios` (
+  `id` int(255) NOT NULL,
+  `nombre` varchar(100) NOT NULL,
+  `apellidos` varchar(100) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `fecha` date NOT NULL,
+  PRIMARY KEY (id),
+  UNIQUE (email)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+CREATE TABLE `Notas` (
+  `id` int(255) NOT NULL,
+  `usuario_id` int (255) NOT NULL,
+  `titulo` varchar(100) NOT NULL,
+  `descripcion` varchar(255) NOT NULL,
+  `fecha` date NOT NULL
+  PRIMARY KEY (id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+ALTER TABLE Notas ADD FOREIGN KEY (usuario_id) REFERENCES usuario (id) ON DELETE CASCADE ON UPDATE CASCADE;
